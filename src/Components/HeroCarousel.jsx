126ccb11eb16e849f0c579682c96333a5465cd37
Luka Ilashvili <ilashvililuka65@gmail.com>
2026-02-04 20:28:08 +0400

    carousel fully working

--- src/Components/HeroCarousel.jsx
+++ src/Components/HeroCarousel.jsx
@@ -1,12 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { fetchPopularMovies } from "../api";
 import "../App.css";
-import MovieCard from "./MovieCard";
+
 
 
 function HeroCarousel() {
     const [movies, setMovies] = useState([]);
     const [index, setIndex] = useState(0);
+    const IntervalRef = useRef(null);
+
+    const startAutoPlay = () => {
+        IntervalRef.current = setInterval(() => {
+            setIndex(prev => (prev + 1) % movies.length);
+        }, 3000);
+    }
+
+    const stopAutoPlay = () => {
+        if (IntervalRef.current) {
+            clearInterval(IntervalRef.current);
+        }}
 
     useEffect(() => {
         fetchPopularMovies().then(data => {
@@ -15,30 +27,34 @@ function HeroCarousel() {
     }, []);
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setIndex(prev => (prev + 1) % movies.length);
-        }, 3000);
-
-        return () => clearInterval(timer);
+        if (!movies.length) return;
+        startAutoPlay();
+        return () => stopAutoPlay();
     }, [movies]);
+
+     
 
     if (!movies.length) return null;
 
     const movie = movies[index];
 
     const prevSlide = () => {
+        stopAutoPlay();
         setIndex(prev => (prev === 0 ? movies.length -1 : prev - 1))
+        startAutoPlay();
     }
 
     const nextSlide = () => {
+        stopAutoPlay();
         setIndex(prev => (prev === movies.length - 1 ? 0 : prev + 1))
+        startAutoPlay();
     }
 
     return (
         <div className="hero" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}}>
             <div className="heroOverlay">
                 <h1>{movie.title}</h1>
-                <p>{movie.overview}</p>
+                <p>Rating: {movie.vote_average}</p>
                 <button className="arrow left" onClick={prevSlide}>←</button>
                 <button className="arrow right" onClick={nextSlide}>→</button>
             </div>
