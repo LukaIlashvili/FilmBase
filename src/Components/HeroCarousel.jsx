6a1d516addd4c61f56a2a458757ca05bd3e3406d
Luka Ilashvili <ilashvililuka65@gmail.com>
2026-02-04 19:53:18 +0400

    carousel arrows

--- src/Components/HeroCarousel.jsx
+++ src/Components/HeroCarousel.jsx
@@ -10,7 +10,7 @@ function HeroCarousel() {
 
     useEffect(() => {
         fetchPopularMovies().then(data => {
-            setMovies(data.results.slice(0, 5));
+            setMovies(data.results.slice(0, 6));
         })
     }, []);
 
@@ -26,11 +26,21 @@ function HeroCarousel() {
 
     const movie = movies[index];
 
+    const prevSlide = () => {
+        setIndex(prev => (prev === 0 ? movies.length -1 : prev - 1))
+    }
+
+    const nextSlide = () => {
+        setIndex(prev => (prev === movies.length - 1 ? 0 : prev + 1))
+    }
+
     return (
         <div className="hero" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}}>
             <div className="heroOverlay">
                 <h1>{movie.title}</h1>
                 <p>{movie.overview}</p>
+                <button className="arrow left" onClick={prevSlide}>←</button>
+                <button className="arrow right" onClick={nextSlide}>→</button>
             </div>
             </div>
     );
